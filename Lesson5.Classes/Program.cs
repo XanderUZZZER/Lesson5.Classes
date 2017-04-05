52af70ab26f12420da7867a17de97f30ddaafd3d
Oleksandr <xander_uzzzer@mail.ru>
2017-04-05 21:06:22 +0300

    0504172106

--- Lesson5.Classes/Program.cs
+++ Lesson5.Classes/Program.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shapes;
 
-namespace Lesson5.Classes
+namespace ConsoleApplication
 {
-    class Program
+    class Program: Shape
     {
+        private void YU()
+        {
+            H = 17;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Lesson 5");
             
+            Shape shape = new Shape();
+            shape.Draw();
+
+            Shape shape1 = new Shape(5, 8);
+            shape1.Draw();
+
+            var triangle = new Triangle();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Shapes/Shape.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Shape
+    {
+        internal protected int H;
+
+        private int _a;
+        private int _b;
+        private Color _color;
+
+        public Shape()
+        {
+            Console.WriteLine(nameof(Shape));
+        }
+
+        public Shape(int a, int b) : this()
+        {
+            _a = a;
+            _b = b;
+            Draw();
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"A:{_a} B:{_b}");
+        }
+        public void Draw(int t)
+        {
+            Console.WriteLine($"A:{_a} B:{_b}");
+        }
+        public void Draw(string t)
+        {
+            Console.WriteLine($"A:{_a} B:{_b}");
+        }
+
+        public static void DrawPerfect()
+        {
+            Console.WriteLine("Static method");
+        }
+        protected virtual void DrawA()
+        {
+            Console.WriteLine(_a);
+        }
+        private void DrawB()
+        {
+            Console.WriteLine(_b);
+        }
+        public int CalculateSquare(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
