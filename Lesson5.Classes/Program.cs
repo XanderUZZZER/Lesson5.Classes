17c460b3509d0e5477937704920d4fc7464dc74d
Oleksandr <xander_uzzzer@mail.ru>
2017-04-05 19:22:16 +0300

    0504171922

--- Lesson5.Classes/Program.cs
+++ Lesson5.Classes/Program.cs
@@ -11,18 +11,7 @@ namespace Lesson5.Classes
         static void Main(string[] args)
         {
             Console.WriteLine("Lesson 5");
-
-            TimeSpan duration = new TimeSpan(1, 12, 23, 62);
-
-            string output12 = null;
-            output12 = "Time of Travel: " + duration.ToString("%d") + " days";
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine(output12);
-            output12 = "Time of Travel: " + duration.ToString(@"dd\.hh\:mm\:ss");
-            Console.WriteLine(output12);
-
-            Console.WriteLine("Time of Travel: {0:%d} day(s)", duration);
-            Console.WriteLine("Time of Travel: {0:dd\\.hh\\:mm\\:ss} days", duration);
+            
         }
     }
 }
